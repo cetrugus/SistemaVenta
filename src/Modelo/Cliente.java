af130ec8f2f4ed5aa05df540b2726c4f2c6c3393
cetrugus <celisgustavoa@gmail.com>
2026-04-13 16:01:24 -0500

    creación del software

---
+++ src/Modelo/Cliente.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Tavo
+ */
+public class Cliente {
+    private int id;
+    private int nit;
+    private String nombre;
+    private String telefono;
+    private String direccion;
+    private String razon;
+    private int estado;
+
+    public Cliente() {
+    }
+
+    public Cliente(int id, int nit, String nombre, String telefono, String direccion, String razon) {
+        this.id = id;
+        this.nit = nit;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.razon = razon;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNit() {
+        return nit;
+    }
+
+    public void setNit(int nit) {
+        this.nit = nit;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getRazon() {
+        return razon;
+    }
+
+    public void setRazon(String razon) {
+        this.razon = razon;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    
+    
+}
---
+++ src/Modelo/ClienteDAO.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Tavo
+ */
+public class ClienteDAO {
+    
+    Conexion cn = new Conexion();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    
+    public boolean RegistrarCliente(Cliente cl){
+        String sql = "INSERT INTO clientes (nit, nombre, telefono, direccion, razon) VALUES (?,?,?,?,?)";
+        try {
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, cl.getNit());
+            ps.setString(2, cl.getNombre());
+            ps.setString(3, cl.getTelefono());
+            ps.setString(4, cl.getDireccion());
+            ps.setString(5, cl.getRazon());
+            ps.execute();
+            return true;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.toString());
+            return false;
+        }finally{
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.out.println(e.toString());
+            }
+        }
+    }
+    
+    public List ListarCliente(){
+        List<Cliente> ListaCl = new ArrayList();
+        String sql = "SELECT * FROM clientes";
+        try {
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()){
+                Cliente cl = new Cliente();
+                cl.setId(rs.getInt("id"));
+                cl.setNit(rs.getInt("nit"));
+                cl.setNombre(rs.getString("nombre"));
+                cl.setTelefono(rs.getString("telefono"));
+                cl.setDireccion(rs.getString("direccion"));
+                cl.setRazon(rs.getString("razon"));
+                cl.setEstado(rs.getInt("estado"));
+                ListaCl.add(cl);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        return ListaCl;
+    }
+    
+    public boolean EliminarCliente(int id) {
+    String sql = "UPDATE clientes SET estado = CASE WHEN estado = 1 THEN 0 ELSE 1 END WHERE id = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.toString());
+            }
+        }
+    }
+}
